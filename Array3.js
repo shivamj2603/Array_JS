93453b5bcb8b24b9c0e6cd48fbab512c40cbf3a2
shivamj2603 <shivamj2603@gmail.com>
2020-11-11 06:50:49 +0530

    Modified:Program to get prime factorials

--- Array3.js
+++ Array3.js
@@ -1,10 +1,17 @@
-var num = process.argv[2];
+var num = parseInt(process.argv[2]);
 var factors = new Array();
-for (var i = 1; i <= num; i++) {
-    //if number is completely divisible by 'i' then 'i' is a factor
-    if (num % i == 0) {
+while (num % 2 == 0) {
+    factors.push(2);
+    num = num / 2;
+}
+for (var i = 3; i <= num * num; i = i + 2) {
+    while (num % i == 0) {
+        num = num / i;
         factors.push(i);
     }
+}
+if (num > 2) {
+    factors.push(2);
 }
 //Print the factors
 console.log("The factors are :");
